db2658381b95383ac766d72ce4f97cb4712308f2
NoMaidenz <deathblossom247@gmail.com>
2023-01-20 18:46:12 -0500

    1

---
+++ vite-project/JS/items.js
@@ -0,0 +1,65 @@
+const items = [
+  {
+    name: "Free 20 min 1:1 VOD",
+    image: "../overwatchCoachingimg.jpg",
+    price: 0,
+    categories: ["Oneonone", "VOD", "Free"],
+  },
+
+  {
+    name: "1:1 Private Vod Review 1hr",
+    image: "../overwatchCoachingimg.jpg",
+    price: 9.99,
+    categories: ["Oneonone", "VOD", "Paid", "Onehour"],
+  },
+
+  {
+    name: "1:1 Private Vod Review 2hr",
+    image: "../overwatchCoachingimg.jpg",
+    price: 14.99,
+    categories: ["Oneonone", "VOD", "Paid", "Twohour"],
+  },
+
+  {
+    name: "Team Vod Review 1hr",
+    image: "../overwatchCoachingimg.jpg",
+    price: 14.99,
+    categories: ["Team", "VOD", "Paid", "Onehour"],
+  },
+
+  {
+    name: "Team Vod Review 2hr",
+    image: "../overwatchCoachingimg.jpg",
+    price: 24.99,
+    categories: ["Team", "VOD", "Paid", "Twohour"],
+  },
+
+  {
+    name: "Live 1:1 Coaching 1hr",
+    image: "../overwatchCoachingimg.jpg",
+    price: 19.99,
+    categories: ["Oneonone", "Live", "Paid", "Onehour"],
+  },
+
+  {
+    name: "Live 1:1 Coaching 2hr",
+    image: "../overwatchCoachingimg.jpg",
+    price: 34.99,
+    categories: ["Oneonone", "Live", "Paid", "Twohour"],
+  },
+
+  {
+    name: "Team Scrim Coaching 1hr",
+    image: "../overwatchCoachingimg.jpg",
+    price: 15.99,
+    categories: ["Team", "Live", "Paid", "Onehour"],
+  },
+
+  {
+    name: "Team Scrim Coaching 1hr",
+    image: "../overwatchCoachingimg.jpg",
+    price: 29.99,
+    categories: ["Team", "Live", "Paid", "Onehour"],
+  },
+];
+export { items };
---
+++ vite-project/JS/main.js
@@ -0,0 +1,63 @@
+import "../styles/style.css";
+import { DOM } from "./DOM";
+import { items } from "./items";
+DOM.thembtn.addEventListener("click", function () {
+  if (document.body.classList.contains("cool")) {
+    document.body.classList.add("warm");
+    document.body.classList.remove("cool");
+    DOM.thembtn.textContent = "Night Theme";
+  } else {
+    document.body.classList.add("cool");
+    document.body.classList.remove("warm");
+    DOM.thembtn.textContent = "Morning Theme";
+  }
+});
+function createItem(name, price, image) {
+  DOM.items.insertAdjacentHTML(
+    "afterbegin",
+    `<div class="item"> <h3> ${name} </h3> <img id="img" src="${image}" alt="An image of ${name}" /> <p id="price">Price: $${price}</p> <button id="purchaseBtn">Purchase</button></div>`
+  );
+}
+function categoryFilter(category) {
+  DOM.items.innerHTML = "";
+  const filtered = items.filter((item) =>
+    item.categories.includes(`${category}`)
+  );
+  filtered.forEach((item) => createItem(item.name, item.price, item.image));
+}
+items.forEach((item) => createItem(item.name, item.price, item.image));
+
+DOM.FreeFilter.addEventListener("click", function () {
+  categoryFilter("Free");
+});
+
+DOM.TeamFilter.addEventListener("click", function () {
+  categoryFilter("Team");
+});
+
+DOM.VODFilter.addEventListener("click", function () {
+  categoryFilter("VOD");
+});
+
+DOM.OnehourFilter.addEventListener("click", function () {
+  categoryFilter("Onehour");
+});
+
+DOM.OneononeFilter.addEventListener("click", function () {
+  categoryFilter("Oneonone");
+});
+
+DOM.PaidFilter.addEventListener("click", function () {
+  categoryFilter("Paid");
+});
+
+DOM.TwohourFilter.addEventListener("click", function () {
+  categoryFilter("Twohour");
+});
+DOM.LiveFilter.addEventListener("click", function () {
+  categoryFilter("Live");
+});
+DOM.resetButton.addEventListener("click", function () {
+  DOM.items.innerHTML = "";
+  items.forEach((item) => createItem(item.name, item.price, item.image));
+});
